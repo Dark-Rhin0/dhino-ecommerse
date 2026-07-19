78a0e06bc896d2dddd8914e4daca80ffa7db889b
Dark-Rhin0 <voletiendat09@gmail.com>
2026-07-19 23:35:29 +0700

    giao diện banner trong trang our shop

--- src/components/ContentsideBar/Login/Login.jsx
+++ src/components/ContentsideBar/Login/Login.jsx
@@ -1,27 +1,27 @@
-import InputCommon from "@components/InputCommon/InputCommon";
-import styles from "./styles.module.scss";
-import Button from "@components/Button/Button"
-import { useFormik } from "formik";
+import InputCommon from '@components/InputCommon/InputCommon';
+import styles from './styles.module.scss';
+import Button from '@components/Button/Button';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useContext, useState } from "react";
-import { ToastContext } from "@/contexts/ToastProvider";
-import { register, signIn, getInfo } from "@/apis/authService";
-import Cookies from "js-cookie";
-import { SideBarContext } from "@/contexts/SideBarProvider";
-import { StoreContext } from "@/contexts/storeProvider";
+import { useContext, useState } from 'react';
+import { ToastContext } from '@/contexts/ToastProvider';
+import { register, signIn, getInfo } from '@/apis/authService';
+import Cookies from 'js-cookie';
+import { SideBarContext } from '@/contexts/SideBarProvider';
+import { StoreContext } from '@/contexts/storeProvider';
 
 function Login() {
-    const {container, title, boxRememberme, lostPW} = styles;
+    const { container, title, boxRememberme, lostPW } = styles;
     const [isRegister, setIsRegister] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const {toast} = useContext(ToastContext)
-    const {setIsOpen} = useContext(SideBarContext);
+    const { toast } = useContext(ToastContext);
+    const { setIsOpen } = useContext(SideBarContext);
     const { setUserId } = useContext(StoreContext);
 
     const formik = useFormik({
         initialValues: {
             email: '',
-            password: '',
+            password: ''
         },
         validationSchema: Yup.object({
             email: Yup.string()
@@ -31,47 +31,45 @@ function Login() {
                 .min(6, 'Password must be atleast 6 letters')
                 .required('Password is required'),
             cfmpassword: Yup.string().oneOf(
-                [Yup.ref('password'),null],
+                [Yup.ref('password'), null],
                 'Passwords must match'
             )
         }),
 
+        onSubmit: async values => {
+            if (isLoading) return;
 
-        onSubmit: async (values) => {
-
-            if(isLoading) return;
-
-            const {email: username, password} = values;
+            const { email: username, password } = values;
             setIsLoading(true);
 
-            if(isRegister) {
-                await register({username, password})
-                .then((res) => {
-                    toast.success(res.data.message);
-                    setIsLoading(false)
-                })
-                .catch((err) => {
-                    toast.error(err.response.data.message);
-                    setIsLoading(false);
-                });
+            if (isRegister) {
+                await register({ username, password })
+                    .then(res => {
+                        toast.success(res.data.message);
+                        setIsLoading(false);
+                    })
+                    .catch(err => {
+                        toast.error(err.response.data.message);
+                        setIsLoading(false);
+                    });
             }
 
-            if(!isRegister) {
-                await signIn({username, password})
-                .then((res) => {
-                    setIsLoading(false);
-                    const {id, token, refreshToken} = res.data;
-                    setUserId(id);
-                    Cookies.set('token', token);
-                    Cookies.set('refreshToken', refreshToken);
-                    Cookies.set('userId', id);
-                    toast.success('Sign in successfully!');
-                    setIsOpen(false);
-                })
-                .catch((err) => {
-                    setIsLoading(false);
-                    toast.error('Sign in failed!');
-                });
+            if (!isRegister) {
+                await signIn({ username, password })
+                    .then(res => {
+                        setIsLoading(false);
+                        const { id, token, refreshToken } = res.data;
+                        setUserId(id);
+                        Cookies.set('token', token);
+                        Cookies.set('refreshToken', refreshToken);
+                        Cookies.set('userId', id);
+                        toast.success('Sign in successfully!');
+                        setIsOpen(false);
+                    })
+                    .catch(err => {
+                        setIsLoading(false);
+                        toast.error('Sign in failed!');
+                    });
             }
         }
     });
@@ -81,26 +79,23 @@ function Login() {
         formik.resetForm();
     };
 
-
-    return ( 
+    return (
         <div className={container}>
-            <div className={title}>
-                {isRegister ? 'SIGN UP' : 'SIGN IN'}
-            </div>
+            <div className={title}>{isRegister ? 'SIGN UP' : 'SIGN IN'}</div>
 
             <form onSubmit={formik.handleSubmit}>
                 <InputCommon
                     id='email'
-                    label="Email"
-                    type="text"
+                    label='Email'
+                    type='text'
                     isRequired
                     formik={formik}
                 />
 
                 <InputCommon
                     id='password'
-                    label="Password"
-                    type="password"
+                    label='Password'
+                    type='password'
                     isRequired
                     formik={formik}
                 />
@@ -108,47 +103,47 @@ function Login() {
                 {isRegister && (
                     <InputCommon
                         id='cfmpassword'
-                        label="Confirm password"
-                        type="password"
+                        label='Confirm password'
+                        type='password'
                         isRequired
                         formik={formik}
                     />
                 )}
 
-
                 {!isRegister && (
                     <div className={boxRememberme}>
-                        <input type="checkbox" />
+                        <input type='checkbox' />
                         <span>Remember me</span>
                     </div>
                 )}
 
-                <Button 
-                    content={isLoading
-                        ? 'LOADING...'
-                        : isRegister 
-                        ? 'REGISTER' 
-                        : 'LOGIN'
-                    } 
+                <Button
+                    content={
+                        isLoading
+                            ? 'LOADING...'
+                            : isRegister
+                              ? 'REGISTER'
+                              : 'LOGIN'
+                    }
                     type='submit'
                 />
             </form>
 
-            <Button 
-                content={isRegister? 
-                    'Already have an account?' : "Don't have an accuont?"
-                } 
+            <Button
+                content={
+                    isRegister
+                        ? 'Already have an account?'
+                        : "Don't have an accuont?"
+                }
                 type='submit'
-                isPrimary = {false}
-                style = {{marginTop: '10px',}}
-                onClick = {handleToggle}
+                isPrimary={false}
+                style={{ marginTop: '10px' }}
+                onClick={handleToggle}
             />
 
             {!isRegister && <div className={lostPW}>Lost your password?</div>}
-            
         </div>
-        
-     );
+    );
 }
 
 export default Login;
